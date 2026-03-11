519545efe244c2c9c8129cd09ef4d1598669f8a0
Gr-1n <l.taavares@gmail.com>
2026-03-11 16:04:48 -0300

    Criada Ordem de Servico

--- oficina/models.py
+++ oficina/models.py
@@ -19,3 +19,33 @@ class Equipamento(models.Model):
     def __str__(self):
         return f"{self.tipo} - {self.marca} {self.modelo}"
 
+class OrdemServico(models.Model):
+
+    Status_CHOICES = [
+        ('recebido', 'Recebido'),
+        ('diagnostico', 'Diagnóstico'),
+        ('aprovacao', 'Aguardando aprovação'),
+        ('reparo', 'Em reparo'),
+        ('finalizado', 'Finalizado'),
+        ('entregue', 'Entregue'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    equipamento = models.ForeignKey(Equipamento, on_delete=models.CASCADE)
+
+    problema = models.TextField()
+    diagnostico = models.TextField(blank=True)
+
+    valor = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='recebido'
+    )
+
+    data_entrada = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"OS #{self.id} - {self.cliente}"
