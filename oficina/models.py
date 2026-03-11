707d96d6b2c110d2ef7bb8984f72b0656351d4df
Gr-1n <l.taavares@gmail.com>
2026-03-11 16:01:02 -0300

    Inserido Equipamentos

--- oficina/models.py
+++ oficina/models.py
@@ -8,4 +8,14 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class Equipamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    numero_serie = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.tipo} - {self.marca} {self.modelo}"
 
