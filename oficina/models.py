0ffbdabf8772027ec94032cc49ccb0c7b539aa69
Gr-1n <l.taavares@gmail.com>
2026-03-11 15:50:38 -0300

    Criacao da oficina

---
+++ oficina/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    endereco = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nome
