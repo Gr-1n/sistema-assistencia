4bcfd4e53375fc45f91be975789015f3f623d839
Gr-1n <l.taavares@gmail.com>
2026-03-11 16:36:49 -0300

    Telas adicionadas

--- oficina/urls.py
+++ oficina/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.dashboard, name='dashboard'),
+
+    path('clientes/', views.clientes, name='clientes'),
+
+    path('equipamentos/', views.equipamentos, name='equipamentos'),
+
+    path('ordens/', views.ordens, name='ordens'),
+
 ]
--- oficina/views.py
+++ oficina/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Cliente, Equipamento, OrdemServico
 
 
 def dashboard(request):
     return render(request, 'oficina/dashboard.html')
+
+
+def clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'oficina/clientes.html', {'clientes': clientes})
+
+
+def equipamentos(request):
+    equipamentos = Equipamento.objects.all()
+    return render(request, 'oficina/equipamentos.html', {'equipamentos': equipamentos})
+
+
+def ordens(request):
+    ordens = OrdemServico.objects.all()
+    return render(request, 'oficina/ordens.html', {'ordens': ordens})
