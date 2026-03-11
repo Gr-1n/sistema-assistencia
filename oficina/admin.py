aefee22d3b13f7e1850a484c74646f58bf0da6f9
Gr-1n <l.taavares@gmail.com>
2026-03-11 16:13:24 -0300

    Modelos inseridos no painel

--- oficina/admin.py
+++ oficina/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Equipamento, OrdemServico
 
 admin.site.register(Cliente)
+admin.site.register(Equipamento)
+admin.site.register(OrdemServico)
